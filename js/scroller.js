0e1661894032f260ed565e1ae30dd0d47e63f951
jrah <jjames.home@gmail.com>
2016-07-27 12:14:45 +0200

    Remove redundant JavaScript
    Ref #27
    Ref #29

--- js/scroller.js
+++ js/scroller.js
@@ -1,14 +1,8 @@
 $(window).scroll(function() {
 if ($(this).scrollTop() > 140){
-    console.log("add sticky");
     $('.page-content > nav').addClass("sticky");
   }
   else{
-    console.log("remove sticky");
     $('.page-content > nav').removeClass("sticky");
   }
-});
-
-$("page").bind('scroll', function() {
-  console.log("Second fired");
 });
